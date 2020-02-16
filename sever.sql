8504d0044428a892b9140a416d6c8cdb9a78af2c
UP892527 <UP892527@users.noreply.github.com>
2020-02-16 15:50:13 +0000

    another update
    1. created two test users
    2. updated id fields to work

--- sever.sql
+++ sever.sql
@@ -1,7 +1,7 @@
 create database GymApp;
 
 create table userInfo(
-	user_id int primary key unique,
+	user_id serial primary key,
 	f_name varchar(20) not null,
 	l_name varchar(20) not null,
 	email varchar(50)not null,
@@ -17,12 +17,12 @@ add constraint check_min_length check (length(user_pin) = 4);
 
 create table workout(
 	user_id int references userInfo(user_id),
-	workout_id int primary key unique,
+	workout_id serial primary key,
 	workout_name varchar(10)
 	);
 
 create table workout_record(
-	session_id int primary key unique,
+	session_id serial primary key,
 	session_date date not null,
 	workout_id int references workout(workout_id),
 	push_ups int,
@@ -31,3 +31,9 @@ create table workout_record(
 	chest_press int,
 	bicep_curl int
 	);
+
+INSERT INTO userinfo (f_name, l_name, email, user_pin, weight, height, age, gender)
+	values ('joe', 'blogs', 'an_email@email.com', 1040, 100.05, 179.50, 28, 'male');
+
+INSERT INTO userinfo (f_name, l_name, email, user_pin, weight, height, age, gender)
+	values ('jane', 'biggs', 'email@email.com', 2040, 95.05, 149.50, 28, 'female');
