1d7db67153ee5832ae7bc8858acdf10f42883750
freweini <frena5@yahoo.com>
2020-03-27 09:43:36 +0000

    Update BMICalculator.js

--- BMICalculator.js
+++ BMICalculator.js
@@ -1,8 +1,9 @@
  function bmi () {
 var weight,height;
-var result = weight / (height * height);
+
 var height = Number(document.getElementById("height").value);
 var weight = Number(document.getElementById("weight").value);
+var result = weight / (height * height);
 document.getElementById("result").innerHTML = "Your bmi score is : " + result;
 }
 //module.exports = bmi;
